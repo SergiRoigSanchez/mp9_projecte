b2dc5f007c3138443f2c8dcb433e7bf7b333d527
SergiRoigSanchez <sergiroig@iesebre.com>
2023-10-23 19:05:40 +0200

    Sync

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saludo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Descripción de la página">
+    <meta name="keywords" content="palabras clave, separadas, por, comas">
+    <meta name="author" content="Nombre del autor">
+    <style>
+        header {
+            background: beige;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1><?= $greeting; ?></h1>
+</header>
+</body>
+</html>
